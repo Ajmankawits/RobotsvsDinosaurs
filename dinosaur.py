81024f5372e356ecb18778581473c762ec9b9e97
Alex <thatsoalex@gmail.com>
2022-03-21 13:59:29 -0500

    fixed a typo in the dinosaur file

--- dinosaur.py
+++ dinosaur.py
@@ -2,7 +2,7 @@ class Dinosaur:
     def __init__(self, name, attack_power):
         self.dinosaur_name = name
         self.dinosaur_attack_power = attack_power
-        self.dinsosaur_health = 1000
+        self.dinosaur_health = 1000
     
     def dinosaur_attack(self, robot):
         pass
