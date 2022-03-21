2227133e82567d24cc35184872b9e6e9466e6c2f
Alex <thatsoalex@gmail.com>
2022-03-21 13:43:10 -0500

    First upload

---
+++ battlefield.py
@@ -0,0 +1,5 @@
+from fleet import Fleet
+from herd import Herd
+class Battlefield:
+    def __init__(self):
+        pass
---
+++ robot.py
@@ -0,0 +1,10 @@
+from weapon import Weapon 
+class Robot:
+    def __init__(self, name):
+        self.robot_name = name
+        self.robot_health = 1000
+        self.robot_weapon = Weapon()
+        
+    def robot_attack(self,dinosaur):
+        pass
+    
